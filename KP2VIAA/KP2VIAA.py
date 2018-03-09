f0af729438ef4cc8bd5443bd1efb675b53da31e5
joachimbovin <joachimbovin@hotmail.com>
2018-03-09 15:19:18 +0100

    initial commit

--- KP2VIAA/KP2VIAA.py
+++ KP2VIAA/KP2VIAA.py
@@ -1,11 +1,26 @@
-class KP2VIAA(object):
-    def __init__(self):
-        self.mapping = None
+from json import load, dumps
+from codecs import open
+import psycopg2
+from configparser import ConfigParser
+from pandas import DataFrame
 
-    def read_mapping_VIAA_to_KP(self):
+
+class KP2VIAA(object):
+    def __init__(self, path_to_dbcfg="resources/db.cfg"):
+        self.mapping = None
+        self.path_to_dbcfg = path_to_dbcfg
+        self.cfg = ConfigParser()
+        self.cfg.read(self.path_to_dbcfg)
+        self.knst = psycopg2.connect(host=self.cfg['db']['host'],
+                                     port=self.cfg['db']['port'],
+                                     database=self.cfg['db']['db'],
+                                     user=self.cfg['db']['user'],
+                                     password=self.cfg['db']['pwd'])
+        self.knst.set_client_encoding('UTF-8')
+
+    def read_mapping_viaa_to_kp(self):
         """
         This function reads in the handmade mapping from VIAA object ids to Kunstenpunt production ids and show ids
-
         {
           viaa_id:
             {
@@ -16,26 +31,108 @@ class KP2VIAA(object):
         """
         self.mapping = {
             "viaa_id": {
-                "kp_productie_id": 449364,
+                "kp_productie_id": 429013,
                 "kp_show_id": "kp_show_id"
             }
         }
 
-    def get_KP_metadata_for_VIAA_id(self, viaa_id):
+    def get_access_database(self):
         """
-        Returns the kunstenpunt metadata for a specific viaa_id. The viaa id is mapped to a kunstenpunt production id
-        and a kunstenpunt show id
+        Gets access to the KP database
+        :return: a cursor object to interact with the MySQL server using a MySQLConnection object.
+        """
+        cur = self.knst.cursor()
+        return cur
+
+    def get_kp_metadata_for_viaa_id(self, viaa_id):
+        """
+        Returns the kunstenpunt metadata about the production for a specific viaa_id. The viaa id is mapped to a kunstenpunt production id
+        and a kunstenpunt show id.
         :param viaa_id: the original viaa id
-        :return: a pandas object containing all the kunstenpunt metadata for the production and potentially the show
+        :return: A pandas object containing all the kunstenpunt metadata for the production and potentially the show.
         """
+        cur = self.get_access_database()
         productie_id = self.mapping[viaa_id]["kp_productie_id"]
         sql = """
-        SELECT * FROM production.productions WHERE productions.id == {0}
+        SELECT pr.title, seasons.name, pr.rerun_of_id
+        
+        FROM production.productions AS pr
+        
+        JOIN production.seasons AS seasons
+        ON pr.season_id = seasons.id
+
+        WHERE pr.id={0}
+
         """.format(productie_id)
-        return sql
+        cur.execute(sql)
+        rosas_productions = cur.fetchall()
+        df_kp_general = DataFrame(rosas_productions, columns=['name', 'season', 'rerun'])
+        return df_kp_general
+
+    def get_kp_metadata_functies_for_viaa_id(self, viaa_id):
+        """
+        Returns the KP metadata about the persons and their function based on the VIAA ID.
+        :param viaa_id:
+        :return: A pandas object containing all the kunstenpunt metadata for the persons and their functions.
+        """
+        cur = self.get_access_database()
+        productie_id = self.mapping[viaa_id]["kp_productie_id"]
+        sql2 = """
+        SELECT people.first_name, people.name, fun.name_nl, pr.id
+        FROM production.productions AS pr
+        JOIN production.relationships AS rel
+        ON pr.id = rel.production_id
+        JOIN production.people as people
+        ON rel.person_id = people.id
+        JOIN production.functions AS fun
+        ON rel.function_id = fun.id
+        WHERE pr.id={0}
+          """.format(productie_id)
+        cur.execute(sql2)
+        rosas_productions = cur.fetchall()
+        df_kp_people = DataFrame(rosas_productions, columns=['first name', 'name', 'function', 'production id'])
+        df_kp_people["full name"] = df_kp_people[['first name', 'name']].apply(lambda x: ' '.join(x), axis=1)
+        df_kp_people = df_kp_people.drop(['first name', 'name'], axis=1)
+        return df_kp_people
+
+    def get_kp_metadata_organisaties_for_viaa_id(self, viaa_id):
+        """
+        Returns the KP metadata about the organisations and their function based on the VIAA ID.
+        :param viaa_id:
+        :return:
+        """
+        cur = self.get_access_database()
+        productie_id = self.mapping[viaa_id]["kp_productie_id"]
+        sql3 = """
+        SELECT organisations.name, functions.name_nl
+        FROM production.productions AS pr
+        JOIN production.relationships AS rel_prod_org
+        ON pr.id = rel_prod_org.production_id
+        JOIN production.organisations as organisations
+        ON rel_prod_org.organisation_id = organisations.id
+        JOIN production.functions as functions
+        ON rel_prod_org.function_id = functions.id
+        WHERE pr.id={0}
+        """.format(productie_id)
+        cur.execute(sql3)
+        rosas_productions = cur.fetchall()
+        df_kp_organisations = DataFrame(rosas_productions, columns=['organisatie', 'functie'])
+        return df_kp_organisations
+
+    def map_kp_to_viaa(self):
+        """
+        Reads the Kunstenpunt metadata and appends this to an XML file
+        :return: XML file
+        """
+        with open("resources/metadata_mapping.json", "r", "utf-8") as f:
+            mapping = load(f)
+        print(dumps(mapping, indent=4))
 
 
 if __name__ == "__main__":
     kp2viaa = KP2VIAA()
-    kp2viaa.read_mapping_VIAA_to_KP()
-    print(kp2viaa.get_KP_metadata_for_VIAA_id("viaa_id"))
+    kp2viaa.read_mapping_viaa_to_kp()
+    #print(kp2viaa.get_KP_metadata_for_VIAA_id("viaa_id"))
+    print(kp2viaa.get_kp_metadata_functies_for_viaa_id("viaa_id"))
+    #print(kp2viaa.get_KP_metadata_organisaties_for_VIAA_id("viaa_id"))
+    kp2viaa.map_kp_to_viaa()
--- KP2VIAA/tests/KP2VIAA_tests.py
+++ KP2VIAA/tests/KP2VIAA_tests.py
@@ -1,22 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from unittest import TestCase
-from KP2VIAA.KP2VIAA import KP2VIAA
 from pandas import DataFrame
+from KP2VIAA.KP2VIAA import KP2VIAA
+from json import load
+from codecs import open
 
 
 class KP2VIAATests(TestCase):
     def setUp(self):
-        self.kp2viaa = KP2VIAA()
+        self.kp2viaa = KP2VIAA(path_to_dbcfg="../resources/db.cfg")
+        self.maxDiff = None
 
     def test_read_mapping(self):
-        self.kp2viaa.read_mapping_VIAA_to_KP()
+        self.kp2viaa.read_mapping_viaa_to_kp()
         expected_mapping = {
             "viaa_id": {
-                "kp_productie_id": "kp_prod_id",
+                "kp_productie_id": 429013,
                 "kp_show_id": "kp_show_id"
             }
         }
         self.assertDictEqual(self.kp2viaa.mapping, expected_mapping)
 
-    def test_get_metadata_for_vortex_temporum(self):
-        vortex_temporum_metadata = self.kp2viaa.get_KP_metadata_for_VIAA_id("viaa_id")
-        self.assertTrue(isinstance(vortex_temporum_metadata, DataFrame))
+    def test_dbcfg_read_in_correctly(self):
+        self.assertEqual(self.kp2viaa.path_to_dbcfg, "../resources/db.cfg")
+        self.assertTrue("db" in self.kp2viaa.cfg)
+
+    def test_get_metadata_for_mozart(self):
+        self.kp2viaa.read_mapping_viaa_to_kp()
+        mozart_metadata = self.kp2viaa.get_kp_metadata_for_viaa_id("viaa_id")
+        mozart_metadata_expected = DataFrame([
+            {
+                "name": "Mozart/Concert Arias Un Moto di Gioia",
+                "season": "1992-1993",
+                "rerun": None
+            }
+        ], columns=["name", "season", "rerun"])
+        self.assertTrue(mozart_metadata.equals(mozart_metadata_expected))
+
+    def test_get_metadata_for_mozart_functies(self):
+        self.kp2viaa.read_mapping_viaa_to_kp()
+        mozart_metadata_functies = self.kp2viaa.get_kp_metadata_functies_for_viaa_id("viaa_id")
+        with open("../resources/mozart_personen_functies.json", "r", "utf-8") as f:
+            x = load(f, encoding='utf-8')
+        mozart_metadata_expected = DataFrame(data=x, columns=['function', "production id", 'full name'])
+        self.assertEqual(mozart_metadata_functies.to_string(), mozart_metadata_expected.to_string())
+
+    def test_get_metadata_for_mozart_organisaties(self):
+        self.kp2viaa.read_mapping_viaa_to_kp()
+        mozart_metadata_organisaties = self.kp2viaa.get_kp_metadata_organisaties_for_viaa_id("viaa_id")
+        mozart_metadata_expected = DataFrame([
+            {
+                "organisatie": "Rosas",
+                "functie": "gezelschap"
+            }
+        ], columns=["organisatie", "functie"])
+        self.assertTrue(mozart_metadata_organisaties.equals(mozart_metadata_expected))
